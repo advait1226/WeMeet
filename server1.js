17f8e6076f571c8617f369a7dd6f8290e71f9822
JoNTy <zx12345qvm@gmail.com>
2023-06-23 00:57:09 +0530

    jonty initial request

--- server1.js
+++ server1.js
@@ -38,7 +38,7 @@ app.use(express.static('public'));
 app.use(morgan('dev')); 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+
 
 
 //session cookie set up
